8a8d8a11720ecef3827a2591f93ab64bea431677
gthaber <gthaber@mail.ru>
2020-04-27 14:58:09 +0700

    Ver1.0

---
+++ container.cpp
@@ -0,0 +1,48 @@
+
+#include "container.h"
+
+void Read_Container(std::ifstream &stream, container &c) {
+    while(!stream.eof()) {
+        transport *temp_tr = Read_Transport(stream);
+        if (temp_tr == nullptr) return; // Если не считалось, то ошибка
+        element *el = new element{};
+        el->t = temp_tr;
+        containerAdd(el, c);
+    }
+}
+
+void containerAdd(element *el, container&con) {
+    con.size++;
+    if(con.starting == nullptr) {
+        con.starting = con.ending = el;
+    } else {
+        con.ending->forward = el;
+        con.ending = el;
+        el->forward = con.starting;
+    }
+}
+
+void containerInit(container &con) {
+    con.size = 0;
+    con.starting = con.ending = nullptr;
+}
+
+void containerClear(container &con) {
+    element *el = con.starting;
+    for(int i = 0; i < con.size; i++) {
+        delete el->t;
+        element *next = el->forward;
+        delete el;
+        el = next;
+    }
+}
+
+void Out_Container(std::ofstream &stream, container &con) {
+    element *el = con.starting;
+    for(int i = 0; i < con.size; i++) {
+        stream << i+1 << ". ";
+        Out_Transport(stream, el->t);
+        el = el->forward;
+    }
+}
+
---
+++ container.h
@@ -0,0 +1,26 @@
+
+#ifndef PR1_1_CONTAINER_H
+#define PR1_1_CONTAINER_H
+
+#include <iostream>
+#include <fstream>
+#include "transport.h"
+
+struct element {
+    transport* t;
+    element* forward;
+};
+
+struct container {
+    element* starting;
+    element* ending;
+    int size;
+};
+
+void containerClear(container&con);
+void containerInit(container&con);
+void containerAdd(element*, container&con);
+void Read_Container(std::ifstream& stream, container&con);
+void Out_Container(std::ofstream& stream, container&con);
+
+#endif //PR1_1_CONTAINER_H
